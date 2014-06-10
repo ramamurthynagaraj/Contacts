db535b0d14e50146d137a41c7794dbe07f461c83
Nagaraj <Nagaraj@Nagarajs-MacBook-Pro.local>
2014-06-10 23:58:08 +0530

    Styled and added quick contacts on click

--- src/phone/Contacts/ContactsListViewFragment.java
+++ src/phone/Contacts/ContactsListViewFragment.java
@@ -5,27 +5,34 @@ import android.app.LoaderManager;
 import android.content.CursorLoader;
 import android.content.Loader;
 import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.provider.ContactsContract.Contacts;
+import android.provider.ContactsContract.QuickContact;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 
 public class ContactsListViewFragment
         extends Fragment
-        implements LoaderManager.LoaderCallbacks<Cursor> {
+        implements LoaderManager.LoaderCallbacks<Cursor>,
+        AdapterView.OnItemClickListener  {
 
-    private String[] Contacts_Details_Projection = new String[]{
+    private static final String[] CONTACT_DETAILS_PROJECTION = new String[]{
             Contacts._ID,
             Contacts.LOOKUP_KEY,
             Contacts.DISPLAY_NAME_PRIMARY
     };
-    private int CONTACTS_ID_INDEX = 0;
-    private int CONTACTS_LOOKUP_KEY_INDEX = 1;
-    private int CONTACTS_DISPLAY_NAME_INDEX = 2;
+    private static final int CONTACTS_ID_INDEX = 0;
+    private static final int CONTACTS_LOOKUP_KEY_INDEX = 1;
+    private static final int CONTACTS_DISPLAY_NAME_INDEX = 2;
+
     private SimpleCursorAdapter cursorAdapter;
+    private ListView contactsListView;
 
     public ContactsListViewFragment(){}
 
@@ -38,27 +45,32 @@ public class ContactsListViewFragment
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         initializeEmptyContactsList();
+        startLoadingContactsInBackground();
+    }
+
+    private void startLoadingContactsInBackground() {
         getLoaderManager().initLoader(0,null,this);
     }
 
     private void initializeEmptyContactsList() {
-        ListView contactsListView = (ListView) getActivity().findViewById(android.R.id.list);
+        contactsListView = (ListView) getActivity().findViewById(android.R.id.list);
         cursorAdapter = new SimpleCursorAdapter(
                 getActivity(),
                 R.layout.list_item_view,
                 null,
-                new String[] { Contacts_Details_Projection[CONTACTS_DISPLAY_NAME_INDEX] },
+                new String[] { CONTACT_DETAILS_PROJECTION[CONTACTS_DISPLAY_NAME_INDEX] },
                 new int[] { android.R.id.text1 },
                 0);
         contactsListView.setAdapter(cursorAdapter);
+        contactsListView.setOnItemClickListener(this);
     }
 
     @Override
     public Loader<Cursor> onCreateLoader(int id, Bundle args) {
         CursorLoader cursorLoader = new CursorLoader(getActivity());
         cursorLoader.setUri(Contacts.CONTENT_URI);
-        cursorLoader.setProjection(Contacts_Details_Projection);
-        cursorLoader.setSortOrder(Contacts_Details_Projection[CONTACTS_DISPLAY_NAME_INDEX]);
+        cursorLoader.setProjection(CONTACT_DETAILS_PROJECTION);
+        cursorLoader.setSortOrder(CONTACT_DETAILS_PROJECTION[CONTACTS_DISPLAY_NAME_INDEX]);
         return cursorLoader;
     }
 
@@ -74,5 +86,16 @@ public class ContactsListViewFragment
 
     private void updateContactsListItems(Cursor contactItemsToBeReplaced){
         cursorAdapter.swapCursor(contactItemsToBeReplaced);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        SimpleCursorAdapter adapter = (SimpleCursorAdapter) parent.getAdapter();
+        Cursor cursor = adapter.getCursor();
+        cursor.moveToPosition(position);
+        long contactId = cursor.getLong(CONTACTS_ID_INDEX);
+        String contactLookUpKey = cursor.getString(CONTACTS_LOOKUP_KEY_INDEX);
+        Uri contactLookupUri = Contacts.getLookupUri(contactId, contactLookUpKey);
+        QuickContact.showQuickContact(getActivity(),getActivity().findViewById(android.R.id.list),contactLookupUri,QuickContact.MODE_LARGE,null);
     }
 }
