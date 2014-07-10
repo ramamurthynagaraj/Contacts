888bacb401f3aaedb2fd86b7da3328aaffaba083
Nagaraj <Nagaraj@Nagarajs-MacBook-Pro.local>
2014-07-10 18:39:05 +0530

    added icons for calling and messaging

--- src/phone/Contacts/ContactDetailsViewFragment.java
+++ src/phone/Contacts/ContactDetailsViewFragment.java
@@ -2,6 +2,9 @@ package phone.Contacts;
 
 import android.app.Fragment;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -50,22 +53,45 @@ public class ContactDetailsViewFragment extends Fragment {
         }
     }
 
-    private void showPhoneNumber(String phoneNumber) {
+    private void showPhoneNumber(final String phoneNumber) {
         LayoutInflater inflater = LayoutInflater.from(getActivity());
         View contactContentView = inflater.inflate(R.layout.contact_content_view, null);
         TextView phoneNumberTextView = (TextView)contactContentView.findViewById(R.id.phone_number_details);
         phoneNumberTextView.setText(phoneNumber);
-        phoneNumberTextView.setOnClickListener(new View.OnClickListener() {
+        showThemedImage(R.drawable.ic_action_call, R.id.ic_action_call, contactContentView, new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String phoneNumber = ((TextView) v).getText().toString();
-                Intent intentCall = new Intent(Intent.ACTION_CALL);
-                intentCall.setData(Uri.parse("tel:" + phoneNumber));
-                startActivity(intentCall);
+                call(phoneNumber);
+            }
+        });
+        showThemedImage(R.drawable.ic_action_mail, R.id.ic_action_mail, contactContentView, new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                message(phoneNumber);
             }
         });
         LinearLayout detailsLayout = (LinearLayout)getActivity().findViewById(R.id.details_view);
         detailsLayout.addView(contactContentView);
+    }
+
+    private void message(String phoneNumber) {
+        Intent intentCall = new Intent(Intent.ACTION_SENDTO);
+        intentCall.setData(Uri.parse("sms:" + phoneNumber));
+        startActivity(intentCall);
+    }
+
+    private void call(String phoneNumber) {
+        Intent intentCall = new Intent(Intent.ACTION_CALL);
+        intentCall.setData(Uri.parse("tel:" + phoneNumber));
+        startActivity(intentCall);
+    }
+
+    private void showThemedImage(int drawableId, int imageViewId, View parentView, View.OnClickListener clickListener){
+        Drawable drawable = getResources().getDrawable(drawableId);
+        drawable.setColorFilter(Color.GRAY, PorterDuff.Mode.SRC_ATOP);
+        ImageView imageView = (ImageView)parentView.findViewById(imageViewId);
+        imageView.setImageDrawable(drawable);
+        imageView.setOnClickListener(clickListener);
     }
 
     private void showContactName(String contactName) {
