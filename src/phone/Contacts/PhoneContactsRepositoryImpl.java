37df311eb3e8e3461f978d1593069f6eb47f97ed
Nagaraj <Nagaraj@Nagarajs-MacBook-Pro.local>
2015-01-14 23:46:09 +0530

    improving the performance of the app affected by refactoring - Nagaraj

--- src/phone/Contacts/PhoneContactsRepositoryImpl.java
+++ src/phone/Contacts/PhoneContactsRepositoryImpl.java
@@ -21,6 +21,7 @@ public class PhoneContactsRepositoryImpl implements IContactsRepository<Long> {
     public static final String DISPLAY_NAME = ContactsContract.Contacts.DISPLAY_NAME_PRIMARY;
     private static final String HAS_PHONE_NUMBER = ContactsContract.Contacts.HAS_PHONE_NUMBER;
     private static final String PHONE_NUMBER = ContactsContract.CommonDataKinds.Phone.NUMBER;
+    private static final String CONTACT_ID = ContactsContract.CommonDataKinds.Phone.CONTACT_ID;
     public static final String CONTACT_TYPE_MOBILE = "Mobile";
 
     public PhoneContactsRepositoryImpl(Activity activity){
@@ -33,7 +34,9 @@ public class PhoneContactsRepositoryImpl implements IContactsRepository<Long> {
         String whereCondition = _ID + "=?";
         Cursor phoneContactsCursor = parentActivity.getContentResolver().query(ContactsContract.Contacts.CONTENT_URI, null, whereCondition, new String[]{id.toString()}, null);
         if (phoneContactsCursor.moveToFirst()){
-            return getContact(phoneContactsCursor);
+            Contact contact = getContact(phoneContactsCursor, getAllPhoneNumbers());
+            contact.photoUri = getContactPhotoUri(id);
+            return contact;
         }
         return null;
     }
@@ -45,10 +48,13 @@ public class PhoneContactsRepositoryImpl implements IContactsRepository<Long> {
 
     @Override
     public List<Contact> getAll() {
-        Cursor phoneContactsCursor = parentActivity.getContentResolver().query(ContactsContract.Contacts.CONTENT_URI, null, null, null, null);
+        Cursor phoneContactsCursor = parentActivity.getContentResolver()
+                .query(ContactsContract.Contacts.CONTENT_URI, null, null, null, null);
+        List<PhoneNumber> allPhoneNumbers = getAllPhoneNumbers();
+
         List<Contact> phoneContacts = new ArrayList<Contact>();
         while (phoneContactsCursor.moveToNext()){
-            Contact contact = getContact(phoneContactsCursor);
+            Contact contact = getContact(phoneContactsCursor, allPhoneNumbers);
             int hasPhoneNumber = phoneContactsCursor.getInt(phoneContactsCursor.getColumnIndex(HAS_PHONE_NUMBER));
             if (hasPhoneNumber == 1) {
                 phoneContacts.add(contact);
@@ -57,35 +63,41 @@ public class PhoneContactsRepositoryImpl implements IContactsRepository<Long> {
         return phoneContacts;
     }
 
-    private Contact getContact(Cursor phoneContacts) {
-        String name =phoneContacts.getString(phoneContacts.getColumnIndex(DISPLAY_NAME));
+    private Contact getContact(Cursor phoneContacts, List<PhoneNumber> allPhoneNumbers) {
+        String name = phoneContacts.getString(phoneContacts.getColumnIndex(DISPLAY_NAME));
         long id = phoneContacts.getLong(phoneContacts.getColumnIndex(_ID));
         String lookupKey = phoneContacts.getString(phoneContacts.getColumnIndex(LOOKUP_KEY));
         Contact contact = new Contact();
         contact.id = id;
         contact.lookupKey = lookupKey;
         contact.displayName = name;
-        contact.phoneNumber = getAllContactNumbers(id);
+        contact.phoneNumber = getAllContactNumbers(id, allPhoneNumbers);
         contact.contactType = CONTACT_TYPE_MOBILE;
-        contact.photoUri = getContactPhotoUri(id);
         return contact;
     }
 
-    private List<String> getAllContactNumbers(long contactId){
-        String whereCondition = ContactsContract.CommonDataKinds.Phone.CONTACT_ID + "=?";
-        Cursor phoneDataCursor = parentActivity.getContentResolver().query(
-                ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
-                null,
-                whereCondition,
-                new String[]{ String.valueOf(contactId)},
-                null
-        );
-        List<String> phoneNumbers = new ArrayList<String>();
-        while(phoneDataCursor.moveToNext()){
-            String phoneNumber = phoneDataCursor.getString(phoneDataCursor.getColumnIndex(PHONE_NUMBER));
+    private List<String> getAllContactNumbers(long contactId, List<PhoneNumber> phoneNumbers){
+        List<String> contactPhoneNumbers = new ArrayList<String>();
+        for (PhoneNumber number : phoneNumbers){
+            if (contactId == number.contactId){
+                contactPhoneNumbers.add(number.phoneNumber);
+            }
+        }
+        return contactPhoneNumbers;
+    }
+
+    private List<PhoneNumber> getAllPhoneNumbers() {
+        Cursor phoneNumbersCursor = parentActivity.getContentResolver()
+                .query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, null, null, null);
+        List<PhoneNumber> phoneNumbers = new ArrayList<PhoneNumber>();
+        while (phoneNumbersCursor.moveToNext()){
+            PhoneNumber phoneNumber = new PhoneNumber();
+            phoneNumber.contactId = phoneNumbersCursor.getLong(phoneNumbersCursor.getColumnIndex(CONTACT_ID));
+            phoneNumber.phoneNumber = phoneNumbersCursor.getString(phoneNumbersCursor.getColumnIndex(PHONE_NUMBER));
             phoneNumbers.add(phoneNumber);
         }
         return phoneNumbers;
+
     }
 
     public Uri getContactPhotoUri(long contactId){
---
+++ src/phone/Contacts/PhoneNumber.java
@@ -0,0 +1,16 @@
+package phone.Contacts;
+
+import android.net.Uri;
+
+import java.util.List;
+
+/**
+* Created by Nagaraj on 12/01/15.
+*/
+public class PhoneNumber {
+    public long contactId;
+    public String phoneNumber;
+
+    public PhoneNumber() {
+    }
+}
